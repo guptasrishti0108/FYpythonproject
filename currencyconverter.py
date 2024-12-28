6fd876c9ade4632dbef2ccc14f4fce6262825442
guptasrishti0108 <srishti01.gupta08@gmail.com>
2024-12-28 12:56:50 +0530

    basic gui

--- currencyconverter.py
+++ currencyconverter.py
@@ -1,1 +1,30 @@
+import tkinter
+from PIL import Image , ImageTk
+from tkinter import ttk
+# def conversion():
 
+
+root=tkinter.Tk()
+root.title("Currency Converter")
+root.config(bg="light blue")
+root.geometry("300x400")
+imagepath=r"/Users/srishtigupta/Desktop/pythonproject/currencypic.jpg"
+image=Image.open(imagepath)
+image_convert=ImageTk.PhotoImage(image)
+image_label=tkinter.Label(root,image=image_convert)
+image_label.pack(pady=10)
+list1=["IND","PAK","ENG","AUS"]
+label1=tkinter.Label(text="from currency")
+label1.pack()
+combobox1=ttk.Combobox(root,value=list1)
+combobox1.pack()
+list2=["IND","PAK","ENG","AUS"]
+label2=tkinter.Label(text="to currency")
+label2.pack()
+combobox2=ttk.Combobox(root,value=list2)
+combobox2.pack()
+frame=tkinter.Frame(root)
+frame.pack()
+button=tkinter.Button(root,text="convert",bg="pink")#,command=conversion)
+button.pack(pady=30)
+root.mainloop()
